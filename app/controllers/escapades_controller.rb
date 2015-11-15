cff0d477f8404510c7a3f470536fe21055d343f3
Alexis Johnson <alexis.johnson052@gmail.com>
2015-11-15 00:08:59 -0500

    implemented basic CRUD for escapades

--- app/controllers/escapades_controller.rb
+++ app/controllers/escapades_controller.rb
@@ -9,18 +9,40 @@ class EscapadesController < ApplicationController
   end
 
   def new
+    @escapade = Escapade.new
   end
 
   def create
+    @escapade = Escapade.new(escapade_params)
+    if @escapade.save
+      flash[:notice] = "You have successfully posted your new idea!"
+      redirect_to escapade_path(@escapade)
+    else
+      render :new
+    end
   end
 
   def edit
+    @escapade = Escapade.find(params[:id])
   end
 
   def update
+    @escapade = Escapade.find(params[:id])
+    if @escapade.update(escapade_params)
+      flash[:notice] = "You have successfully updated your idea!"
+      redirect_to escapade_path(@escapade)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    Escapade.find(params[:id]).destroy
+    redirect_to escapades_path
   end
 
+  private
+  def escapade_params
+    params.require(:escapade).permit(:title, :image, :description, :start_date, :end_date)
+  end
 end
