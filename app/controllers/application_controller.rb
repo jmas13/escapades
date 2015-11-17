40da2d569939c863f68c1d2cec12692d818c84f8
Alexis Johnson <alexis.johnson052@gmail.com>
2015-11-16 22:08:18 -0500

    improve date matching to parse and display multiple ranges

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :get_optimal_dates
 
-  def get_optimal_dates
-  end
 
 end
